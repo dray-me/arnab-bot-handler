c83165f9563886fe737876d907536fab63d3439f
~ Dray !! <arnabhalder902@gmail.com>
2026-04-03 19:39:07 +0530

    Initial commit via GitFlow

---
+++ commands/prefix/fun/meme.py
@@ -0,0 +1,8 @@
+metadata = {
+    "name": "meme",
+    "description": "Get a random meme",
+    "category": "fun"
+}
+
+async def execute(bot, ctx, *args):
+    await ctx.send("Here is a meme: 😂 (Mock)")
