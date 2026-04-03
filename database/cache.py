e361ff7a3e50e9ec4430c26a146d7fa61d7211fa
~ Dray !! <arnabhalder902@gmail.com>
2026-04-03 19:39:15 +0530

    Initial commit via GitFlow

---
+++ database/cache.py
@@ -0,0 +1,9 @@
+class Cache:
+    def __init__(self):
+        self._data = {}
+
+    def get(self, key):
+        return self._data.get(key)
+
+    def set(self, key, value):
+        self._data[key] = value
